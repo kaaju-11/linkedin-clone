027d3b6deb7b4e67a3c4f6a6d82acac178945840
Kajal Raj <kajal.raj@thewitslab.com>
2025-01-16 14:07:32 +0530

    feat: Protect Route Middleware

--- Backend/controllers/auth.controller.js
+++ Backend/controllers/auth.controller.js
@@ -87,3 +87,12 @@ export const login = async (req, res) => {
     res.clearCookie("jwt-linkedin");
     res.json({message: "Logged out successfully"});
  }
+
+ export const getCurrentUser = async (req, res) => {
+	try {
+		res.json(req.user);
+	} catch (error) {
+		console.error("Error in getCurrentUser controller:", error);
+		res.status(500).json({ message: "Server error" });
+	}
+};
