6e905c25d441e74dc4e81b79cd1ea4973fa25d87
Kajal Raj <kajal.raj@thewitslab.com>
2025-01-17 13:14:10 +0530

    feat: User Route

--- Backend/server.js
+++ Backend/server.js
@@ -1,15 +1,21 @@
 import express from "express"
 import dotenv from "dotenv"
 import authRoutes from "./routes/auth.route.js"
+import userRoutes from "./routes/user.route.js"
 import { connectDb } from "./lib/db.js";
 import cookieParser from "cookie-parser";
 
 dotenv.config();
+
 const app = express();
 const PORT = process.env.PORT || 5000 ;
+
 app.use(express.json());  // parse json request bodies
 app.use(cookieParser());
+
 app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/users", userRoutes);
+
 app.listen(PORT, ()=>{
     console.log(`server running on this ${PORT}`);
     connectDb();
