5254d658a749d85ac1ade9d2d09d363e62df5957
Kajal Raj <kajal.raj@thewitslab.com>
2025-01-17 13:28:21 +0530

    feat: cloudinary setup

--- Backend/controllers/user.controller.js
+++ Backend/controllers/user.controller.js
@@ -62,15 +62,15 @@ export const updateProfile = async (req, res) => {
 
 		// Check for the profile img and banner img => uploaded by cloudinary
 
-		// if (req.body.profilePicture) {
-		// 	const result = await cloudinary.uploader.upload(req.body.profilePicture);
-		// 	updatedData.profilePicture = result.secure_url;
-		// }
+		if (req.body.profilePicture) {
+			const result = await cloudinary.uploader.upload(req.body.profilePicture);
+			updatedData.profilePicture = result.secure_url;
+		}
 
-		// if (req.body.bannerImg) {
-		// 	const result = await cloudinary.uploader.upload(req.body.bannerImg);
-		// 	updatedData.bannerImg = result.secure_url;
-		// }
+		if (req.body.bannerImg) {
+			const result = await cloudinary.uploader.upload(req.body.bannerImg);
+			updatedData.bannerImg = result.secure_url;
+		}
 
 		const user = await User.findByIdAndUpdate(req.user._id, { $set: updatedData }, { new: true }).select(
 			"-password"
