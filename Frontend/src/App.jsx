4d322193c8af3e50df46f0d72faa6f9fa7ed0115
Kajal Raj <kajal.raj@thewitslab.com>
2025-01-18 11:13:29 +0530

    feat: signup ui and api implementation

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,16 +1,18 @@
 import { Route, Routes } from "react-router-dom";
 import Layout from "./components/layout/Layout";
 import HomePage from "./pages/HomePage";
-import SignupPage from "./pages/auth/SignupPage";
 import LoginPage from "./pages/auth/LoginPage";
+import SignUpPage from "./pages/auth/SignupPage";
+import { Toaster } from "react-hot-toast";
 
 function App() {
   return <Layout>
     <Routes>
       <Route path="/" element={<HomePage />} />
-      <Route path="/signup" element={<SignupPage />} />
+      <Route path="/signup" element={<SignUpPage />} />
       <Route path="/login" element={<LoginPage />} />
     </Routes>
+    <Toaster />
   </Layout>;
 }
 
